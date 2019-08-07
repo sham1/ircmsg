9bff1f9955a4501c130ca6e1737534c60901f6be
Jani Juhani Sinervo <jani@sinervo.fi>
2019-08-07 15:57:19 +0300

    Test tag parsing
    This also turns the parsing into a proper state machine.

--- src/parser.c
+++ src/parser.c
@@ -48,7 +48,7 @@ parse_tag(const uint8_t *head,
         size_t value_len = 0;
 
 	const uint8_t *iter;
-	for (iter = head; head < tail; ++iter) {
+	for (iter = head; iter < tail; ++iter) {
 		// Values are split from names with 0x3D '='.
 		if (*iter == 0x3D) {
 			name_len = iter - name_head;
@@ -63,6 +63,22 @@ parse_tag(const uint8_t *head,
 	cbs->on_tag(name_head, name_len, value_head, value_len, user_data);
 }
 
+typedef enum {
+	// These states are "uninterruptable" as in if we encounter CRLF
+	// in any of these states, the parsing fails.
+	SEARCHING_TAGS_PREFIX_COMMAND,
+	PARSING_TAGS,
+	SEARCHING_PREFIX_COMMAND,
+	PARSING_PREFIX,
+	SEARCHING_COMMAND,
+	// These states are "interruptable" as in if we encounter CRLF,
+	// parsing succeeds.
+	PARSING_COMMAND,
+	SEARCHING_PARAMS,
+	PARSING_PARAMS,
+	PARSING_TRAILING_PARAM,
+} parsing_state;
+
 size_t
 ircmsg_parse(const uint8_t *buf,
 	     size_t buf_size,
@@ -72,25 +88,21 @@ ircmsg_parse(const uint8_t *buf,
 	size_t bytes_consumed = 0;
 
 	bool hit_error = false;
-	bool found_message_start = false;
-	bool is_tail_param = false;
-
-	bool found_tags = false;
-	bool parsed_tags = false;
+	parsing_state current_state = SEARCHING_TAGS_PREFIX_COMMAND;
 
 	const uint8_t *head = buf;
 	for (const uint8_t *iter = buf; iter < buf + buf_size; ++iter, ++bytes_consumed) {
 		// If we're not on the last parameter of a command,
 		// which may contain whitespaces, jump to the next
 		// character to seek for the next message token.
-		if (!is_tail_param && is_irc_whitespace (*iter)) {
+		if ((current_state != PARSING_TRAILING_PARAM) && is_irc_whitespace (*iter)) {
 			// Special consideration is needed if we're currently
 			// parsing tags.
-			if (found_tags && (!parsed_tags)) {
+			if (current_state == PARSING_TAGS) {
 				if (head != iter) {
 					parse_tag(head, iter, cbs, user_data);
 					head = iter + 1;
-					parsed_tags = true;
+					current_state = SEARCHING_PREFIX_COMMAND;
 				}
 			} else {
 				head = iter + 1;
@@ -107,7 +119,7 @@ ircmsg_parse(const uint8_t *buf,
 		if (*iter == '\r' || *iter == '\n') {
 			bool was_cr = *iter == '\r';
 			bool was_lf = *iter == '\n';
-			if (found_tags && (!parsed_tags)) {
+			if (current_state == PARSING_TAGS) {
 				cbs->on_error(IRCMSG_ERR_PARSER_UNEXPECTED_END_OF_MESSAGE, user_data);
 				hit_error = true;
 				break;
@@ -116,8 +128,12 @@ ircmsg_parse(const uint8_t *buf,
 				if (was_lf && *(iter + 1) == '\r') {
 					++bytes_consumed;
 					++iter;
-				        if (found_message_start) {
+				        if (current_state >= SEARCHING_PARAMS) {
 						cbs->end_message(user_data);
+					} else if (current_state > SEARCHING_TAGS_PREFIX_COMMAND) {
+						cbs->on_error(IRCMSG_ERR_PARSER_UNEXPECTED_END_OF_MESSAGE, user_data);
+						hit_error = true;
+						break;
 					} else {
 						cbs->on_error(IRCMSG_ERR_PARSER_MESSAGE_NOT_FOUND, user_data);
 						hit_error = true;
@@ -126,8 +142,12 @@ ircmsg_parse(const uint8_t *buf,
 				} else if (was_cr && *(iter + 1) == '\n') {
 					++bytes_consumed;
 					++iter;
-				        if (found_message_start) {
+				        if (current_state >= SEARCHING_PARAMS) {
 						cbs->end_message(user_data);
+					} else if (current_state > SEARCHING_TAGS_PREFIX_COMMAND) {
+						cbs->on_error(IRCMSG_ERR_PARSER_UNEXPECTED_END_OF_MESSAGE, user_data);
+						hit_error = true;
+						break;
 					} else {
 						cbs->on_error(IRCMSG_ERR_PARSER_MESSAGE_NOT_FOUND, user_data);
 						hit_error = true;
@@ -142,8 +162,12 @@ ircmsg_parse(const uint8_t *buf,
 					hit_error = true;
 					break;
 				} else {
-					if (found_message_start) {
+					if (current_state >= SEARCHING_PARAMS) {
 						cbs->end_message(user_data);
+					} else if (current_state > SEARCHING_TAGS_PREFIX_COMMAND) {
+						cbs->on_error(IRCMSG_ERR_PARSER_UNEXPECTED_END_OF_MESSAGE, user_data);
+						hit_error = true;
+						break;
 					} else {
 						cbs->on_error(IRCMSG_ERR_PARSER_MESSAGE_NOT_FOUND, user_data);
 						hit_error = true;
@@ -151,7 +175,7 @@ ircmsg_parse(const uint8_t *buf,
 					break;
 				}
 			} else {
-				if (found_message_start) {
+				if (current_state >= SEARCHING_PARAMS) {
 					cbs->end_message(user_data);
 					break;
 				} else {
@@ -164,8 +188,7 @@ ircmsg_parse(const uint8_t *buf,
 		// IRCv3 introduced so-called "tags" to messages. These tags are optional,
 		// but if they are present, the byte to indicate as much is 0x40 '@'.
 		if (*iter == 0x40) {
-			found_message_start = true;
-			found_tags = true;
+			current_state = PARSING_TAGS;
 			cbs->start_message(user_data);
 			cbs->start_tags(user_data);
 			head = iter + 1;
@@ -173,15 +196,18 @@ ircmsg_parse(const uint8_t *buf,
 			continue;
 		}
 
-		if (found_tags && (!parsed_tags)) {
+		if (current_state == PARSING_TAGS) {
 			if (*iter == ';') {
 				parse_tag(head, iter, cbs, user_data);
 				head = iter + 1;
 			}
+			continue;
+		}
 		}
 	}
 
-	if (!found_message_start && !hit_error) {
+	if ((current_state == SEARCHING_TAGS_PREFIX_COMMAND && !hit_error) ||
+	    (current_state < SEARCHING_PARAMS && !hit_error)) {
 		cbs->on_error(IRCMSG_ERR_PARSER_UNEXPECTED_END_OF_MESSAGE, user_data);
 		hit_error = true;
 	}
--- test/parser_failure.c
+++ test/parser_failure.c
@@ -26,9 +26,54 @@
 #include <string.h>
 #include <stdbool.h>
 #include <ircmsg/parser.h>
+#include <stdio.h>
+
+struct failure_irc_tag
+{
+	char *name;
+	char *value;
+};
+
+static struct failure_irc_tag **
+reallocate_tags_arr(struct failure_irc_tag **arr, size_t count)
+{
+	struct failure_irc_tag **ret = realloc(arr, (count + 1) * sizeof(*arr));
+	ret[count] = NULL;
+	return ret;
+}
+
+static size_t
+count_tags(struct failure_irc_tag **arr)
+{
+        size_t ret = 0;
+	for (struct failure_irc_tag **iter = arr; *iter; ++iter) {
+		++ret;
+	}
+	return ret;
+}
+
+static void
+free_tags_arr(struct failure_irc_tag **arr)
+{
+	if (arr == NULL) return;
+	for (struct failure_irc_tag **iter = arr; *iter; ++iter) {
+		free((*iter)->name);
+		free((*iter)->value);
+		free(*iter);
+	}
+}
+
+struct failure_irc_message
+{
+	struct failure_irc_tag **tags;
+	char *prefix;
+	char *command;
+	char **params;
+};
 
 struct failure_test_struct
 {
+	struct failure_irc_message *msg;
 	bool failed;
 	ircmsg_parser_err_code code;
 };
@@ -39,19 +84,90 @@ failure_test_on_error(ircmsg_parser_err_code error, void *user_data)
 	struct failure_test_struct *test_struct = user_data;
 	test_struct->failed = true;
 	test_struct->code = error;
+
+	if (test_struct->msg != NULL) {
+		free_tags_arr(test_struct->msg->tags);
+		free(test_struct->msg);
+	}
+}
+
+static void
+failure_test_start_message(void *user_data)
+{
+	struct failure_test_struct *test_struct = user_data;
+	// If this fails in this test, something is seriously wrong.
+	// This is not recoverable.
+	test_struct->msg = calloc(1, sizeof(*test_struct->msg));
+}
+
+static void
+failure_test_end_message(void *user_data)
+{
+	struct failure_test_struct *test_struct = user_data;
+	free(test_struct->msg);
+}
+
+static void
+failure_test_start_tags(void *user_data)
+{
+	struct failure_test_struct *test_struct = user_data;
+        struct failure_irc_message *msg = test_struct->msg;
+	msg->tags = reallocate_tags_arr(NULL, 0);
+}
+
+static char *
+allocate_len(const uint8_t *str, size_t len)
+{
+	if (str == NULL) return NULL;
+        char *ret = calloc(len + 1, sizeof(*ret));
+	memcpy(ret, str, len);
+	return ret;
+}
+
+static void
+failure_test_on_tag(const uint8_t *name, size_t name_len,
+		    const uint8_t *esc_value, size_t esc_value_len,
+		    void *user_data)
+{
+	struct failure_test_struct *test_struct = user_data;
+        struct failure_irc_message *msg = test_struct->msg;
+	size_t old_len = count_tags(msg->tags);
+
+	msg->tags = reallocate_tags_arr(msg->tags, old_len + 1);
+	msg->tags[old_len] = calloc(1, sizeof(*msg->tags[old_len]));
+	msg->tags[old_len]->name = allocate_len(name, name_len);
+	msg->tags[old_len]->value = allocate_len(esc_value, esc_value_len);
+}
+
+static void
+failure_test_end_tags(void *user_data)
+{
+	struct failure_test_struct *test_struct = user_data;
+        struct failure_irc_message *msg = test_struct->msg;
+
+	size_t tags_count = count_tags(msg->tags);
+	for (size_t i = 0; i < tags_count; ++i) {
+		char *name = msg->tags[i]->name;
+		char *value = msg->tags[i]->value;
+		if (value) {
+			printf("Tag %s with value %s\n", name, value);
+		} else {
+			printf("Tag %s without value\n", name);
+		}
+	}
 }
 
 static ircmsg_parser_callbacks cbs = {
-	.start_message = NULL,
-	.start_tags = NULL,
-	.on_tag = NULL,
-	.end_tags = NULL,
+	.start_message = failure_test_start_message,
+	.start_tags = failure_test_start_tags,
+	.on_tag = failure_test_on_tag,
+	.end_tags = failure_test_end_tags,
 	.on_prefix = NULL,
 	.on_command = NULL,
 	.start_params = NULL,
 	.on_param = NULL,
 	.end_params = NULL,
-	.end_message = NULL,
+	.end_message = failure_test_end_message,
 
 	.on_error = failure_test_on_error,
 };
@@ -115,6 +231,19 @@ test_whitespace (void **state)
 	assert_true(test_struct->failed);
 	assert_int_equal(test_struct->code, IRCMSG_ERR_PARSER_MESSAGE_NOT_FOUND);
 }
+static void
+test_tags_unexpected_end (void **state)
+{
+	struct failure_test_struct *test_struct = *state;
+	const char *tags_str = "    @test;foo=bar\\s \r\n";
+	size_t consumed = ircmsg_parse((const uint8_t *) tags_str,
+				       strlen(tags_str),
+				       &cbs,
+				       test_struct);
+        assert_int_equal(consumed, 0);
+	assert_true(test_struct->failed);
+	assert_int_equal(test_struct->code, IRCMSG_ERR_PARSER_UNEXPECTED_END_OF_MESSAGE);
+}
 
 int
 main (int argc, char **argv)
@@ -127,6 +256,9 @@ main (int argc, char **argv)
 						failure_setup,
 						failure_teardown),
 		cmocka_unit_test_setup_teardown(test_whitespace,
+						failure_setup,
+						failure_teardown),
+		cmocka_unit_test_setup_teardown(test_tags_unexpected_end,
 						failure_setup,
 						failure_teardown),
 	};
