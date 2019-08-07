7209b03609bcf4aeaf8069329449f021dec38b29
Jani Juhani Sinervo <jani@sinervo.fi>
2019-08-07 14:43:25 +0300

    Do initial work for parsing tags

--- src/parser.c
+++ src/parser.c
@@ -35,6 +35,34 @@ is_irc_whitespace(uint8_t byte)
 	}
 }
 
+static void
+parse_tag(const uint8_t *head,
+	  const uint8_t *tail,
+	  const ircmsg_parser_callbacks *cbs,
+	  void *user_data)
+{
+	const uint8_t *name_head = head;
+	size_t name_len = 0;
+
+	const uint8_t *value_head = NULL;
+        size_t value_len = 0;
+
+	const uint8_t *iter;
+	for (iter = head; head < tail; ++iter) {
+		// Values are split from names with 0x3D '='.
+		if (*iter == 0x3D) {
+			name_len = iter - name_head;
+			value_head = iter + 1;
+		}
+	}
+
+	if (value_head != NULL) {
+		value_len = iter - value_head;
+	}
+
+	cbs->on_tag(name_head, name_len, value_head, value_len, user_data);
+}
+
 size_t
 ircmsg_parse(const uint8_t *buf,
 	     size_t buf_size,
@@ -47,13 +75,26 @@ ircmsg_parse(const uint8_t *buf,
 	bool found_message_start = false;
 	bool is_tail_param = false;
 
+	bool found_tags = false;
+	bool parsed_tags = false;
+
 	const uint8_t *head = buf;
 	for (const uint8_t *iter = buf; iter < buf + buf_size; ++iter, ++bytes_consumed) {
 		// If we're not on the last parameter of a command,
 		// which may contain whitespaces, jump to the next
 		// character to seek for the next message token.
 		if (!is_tail_param && is_irc_whitespace (*iter)) {
-		        head = iter + 1;
+			// Special consideration is needed if we're currently
+			// parsing tags.
+			if (found_tags && (!parsed_tags)) {
+				if (head != iter) {
+					parse_tag(head, iter, cbs, user_data);
+					head = iter + 1;
+					parsed_tags = true;
+				}
+			} else {
+				head = iter + 1;
+			}
 		}
 
 		// The IRC spec specifies that messages are terminated by
@@ -66,6 +107,11 @@ ircmsg_parse(const uint8_t *buf,
 		if (*iter == '\r' || *iter == '\n') {
 			bool was_cr = *iter == '\r';
 			bool was_lf = *iter == '\n';
+			if (found_tags && (!parsed_tags)) {
+				cbs->on_error(IRCMSG_ERR_PARSER_UNEXPECTED_END_OF_MESSAGE, user_data);
+				hit_error = true;
+				break;
+			}
 			if (iter != (buf + buf_size) - 1) {
 				if (was_lf && *(iter + 1) == '\r') {
 					++bytes_consumed;
@@ -112,6 +158,25 @@ ircmsg_parse(const uint8_t *buf,
 					cbs->on_error(IRCMSG_ERR_PARSER_MESSAGE_NOT_FOUND, user_data);
 					hit_error = true;
 				}
+			}
+		}
+
+		// IRCv3 introduced so-called "tags" to messages. These tags are optional,
+		// but if they are present, the byte to indicate as much is 0x40 '@'.
+		if (*iter == 0x40) {
+			found_message_start = true;
+			found_tags = true;
+			cbs->start_message(user_data);
+			cbs->start_tags(user_data);
+			head = iter + 1;
+
+			continue;
+		}
+
+		if (found_tags && (!parsed_tags)) {
+			if (*iter == ';') {
+				parse_tag(head, iter, cbs, user_data);
+				head = iter + 1;
 			}
 		}
 	}
